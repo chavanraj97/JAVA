8e9cd26636bac9dafb7690dce8af05cccf5d9859
chavanraj97 <chavanraj97@gmail.com>
2020-06-11 01:36:57 +0530

    This is just the beginning

---
+++ Assignment_Day1/Table.java
@@ -0,0 +1,17 @@
+public class Table
+{
+	public static void main(String args[])
+	{
+		int a = 5;
+		
+		int i;
+		A:for(i = 1; i <= 10; i++)
+		{
+			int temp;
+			temp = a * i;
+			System.out.println(a  + " * " + i + " = " + temp);	
+		}
+	}
+}
+
+			
